5dabd8090051f28936b190a288ef0b6b0308a585
PavelPavells <hismailforyou@gmail.com>
2023-02-01 01:57:49 +0200

    add main layout, loader layout

---
+++ src/layouts/Main/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <LoaderLayout :isLoading="isLoading">
+    <section class="wrapper">
+      <!-- <Sidebar />
+      <Header /> -->
+      <div class="content">
+        <slot />
+      </div>
+    </section>
+  </LoaderLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BaseIcon } from '@components/ui';
+import LoaderLayout from '../Loader/Loader.vue';
+
+export default defineComponent({
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  components: {
+    LoaderLayout,
+    BaseIcon,
+  },
+});
+</script>
+
+<style>
+@import url('./Main.scss');
+</style>
